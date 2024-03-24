5435777a57075a1b2345c9208730f337e1cf6e64
unbreaking-V <wlad.obezuk@gmail.com>
2024-03-24 12:45:21 +0100

    [#29] Fix length_search.sh

--- src/local_search.sh
+++ src/local_search.sh
@@ -1,32 +1,9 @@
 #!/bin/bash
 
-instances=("kroA100.tsp" "kroB100.tsp")
-start=("random" "regret")
-movements_type=("inner" "inter")
-algos=("0" "1") # greedy - 0 / steepest - 1
-
-visited_combinations=()
-
-#for instance in "${instances[@]}"; do
-#    for algo in "${algos[@]}"; do
-#        for start in "${start[@]}"; do
-#            for movement in "${movements_type[@]}"; do
-#                combination="$instance$start$movement$algo"
-#                if [[ ! " ${visited_combinations[@]} " =~ " $combination " ]]; then
-#                    visited_combinations+=("$combination")
-#                    # echo "Running local search: $instance $start $movement $algo"
-#                    # ./report.sh "$instance" "local" "$start" "$movement" "$algo"
-#                fi
-#            done
-#        done
-#    done
-#done
-
-
 regret_random=("regret" "random")
 inner_inter=("inner" "inter")
 zero_one=("0" "1")
-kro=("kroA100" "kroB100")
+kro=("kroA100.tsp" "kroB100.tsp")
 
 # Loop through all combinations
 for r in "${regret_random[@]}"; do
@@ -35,6 +12,7 @@ for r in "${regret_random[@]}"; do
             for k in "${kro[@]}"; do
                 # Print the combination
                 echo "${r} ${i} ${z} ${k}"
+                ./report.sh "$k" "local" "$r" "$i" "$z"
             done
         done
     done
