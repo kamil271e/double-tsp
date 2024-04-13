e4e0c1eb4c49d8063cd45a0c9563041dda71f745
kamil <kamil271e@gmail.com>
2024-04-13 21:22:21 +0200

    [#30] Add adj_matrix

--- src/utils.cpp
+++ src/utils.cpp
@@ -9,3 +9,15 @@ std::vector<std::string> splitString(const std::string& str, char delimiter) {
     }
     return tokens;
 }
+
+std::vector<std::vector<int>> generate_adj_matrix(std::vector<int> cycle){
+    int n = cycle.size();
+    std::vector<std::vector<int>> adj_matrix(n, std::vector<int>(n, 0));
+    for (int i = 0; i < n; i++){
+        adj_matrix[cycle[i]][cycle[(i+1)%n]] = 1;
+        adj_matrix[cycle[(i+1)%n]][cycle[i]] = -1;
+        adj_matrix[cycle[(i+1)%n]][cycle[i]] = 1;
+        adj_matrix[cycle[i]][cycle[(i+1)%n]] = -1;
+    }
+    return adj_matrix;
+}
