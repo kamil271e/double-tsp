5f6c083486a37a5be79c2b8e914efe601c10d5f1
kamil <kamil271e@gmail.com>
2024-05-23 20:21:46 +0200

    Use clang-format

--- lib/matrix.h
+++ lib/matrix.h
@@ -2,21 +2,21 @@
 #define MATRIX_H
 
 #pragma once
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include <string>
 #include <cmath>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
 
-class Matrix{
+class Matrix {
 public:
-    Matrix();
-    void load_from_path(std::string);
-    void generate_dist_matrix();
-    int calc_distance(int, int, int, int);
-    std::vector<int> x_coord;
-    std::vector<int> y_coord;
-    std::vector<std::vector<int>> dist_matrix;
+  Matrix();
+  void load_from_path(std::string);
+  void generate_dist_matrix();
+  int calc_distance(int, int, int, int);
+  std::vector<int> x_coord;
+  std::vector<int> y_coord;
+  std::vector<std::vector<int>> dist_matrix;
 };
 
 #endif // MATRIX_H
